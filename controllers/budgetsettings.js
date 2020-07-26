712fb77627e16ede5f9c3a72ef31768aae7a00c6
MikaLeppiaho <MikaLeppiaho@users.noreply.github.com>
2020-07-26 13:31:24 +0300

    Päivittäisen budgettisumman haku

--- controllers/budgetsettings.js
+++ controllers/budgetsettings.js
@@ -1,81 +1,87 @@
 //API Endpointit kuukausibudgetin määrittelyä varten
 
-const budgetSettingsRouter = require('express').Router()
-const BudgetSetting = require('../models/budgetsetting')
-const DailyBudget = require('../models/dailybudget')
-const jwt = require('jsonwebtoken')
-const User = require('../models/user')
-const tools = require('../utils/tools')
-const Expense = require('../models/expense')
+const budgetSettingsRouter = require("express").Router();
+const BudgetSetting = require("../models/budgetsetting");
+const DailyBudget = require("../models/dailybudget");
+const jwt = require("jsonwebtoken");
+const User = require("../models/user");
+const tools = require("../utils/tools");
+const Expense = require("../models/expense");
 
 //
-//Hae kaikki budgettiasetukset
+//Hae käyttäjän budgettiasetukset
 //
-budgetSettingsRouter.get('/', async (request, response) => {
-    const settings = await BudgetSetting.find({}).populate('expenses', {description: 1, amount: 1})
-    response.json(settings.map(s => s.toJSON())) 
-})
+budgetSettingsRouter.get("/", async (request, response) => {
+  //Tarkistetaan käyttäjä tokenin avulla
+  const decodedToken = jwt.verify(request.token, process.env.SECRET);
+  if (!request.token || !decodedToken.id) {
+    return response.status(401).json({ error: "token missing or invalid" });
+  }
+  console.log("Decoded token: ", JSON.stringify(decodedToken));
+  const settings = await BudgetSetting.findOne({
+    user: decodedToken.id,
+  }).populate("expenses", {
+    description: 1,
+    amount: 1,
+  });
+  response.json(settings);
+});
 //
 //Poista käyttäjän budgettiasetukset
 //
-budgetSettingsRouter.delete('/', async (request, response) => {
-    const settings = await BudgetSetting.deleteMany({})
-    const dailys = await DailyBudget.deleteMany({})
-    const expenses = await Expense.deleteMany({})
-    response.status(204).end() 
-})
+budgetSettingsRouter.delete("/", async (request, response) => {
+  const settings = await BudgetSetting.deleteMany({});
+  const dailys = await DailyBudget.deleteMany({});
+  const expenses = await Expense.deleteMany({});
+  response.status(204).end();
+});
 
 //
 //Hae käyttäjän budgettiasetukset id parametrillä
 //
-budgetSettingsRouter.get('/:id', async (request, response) => {
-    const id = request.params.id
-    console.log('id: ',id)
-    const settings = await BudgetSetting.findOne({user: id}).populate('expenses', {description: 1, amount: 1})
-    response.json(settings)
-})
-
+budgetSettingsRouter.get("/:id", async (request, response) => {
+  const id = request.params.id;
+  console.log("id: ", id);
+  const settings = await BudgetSetting.findOne({
+    user: id,
+  }).populate("expenses", { description: 1, amount: 1 });
+  response.json(settings);
+});
 
 //Lisää budgettiasetus. Lisäksi endpoint lisää kuluvalle kuukauden jäljelläoleville
 //päiville käytettävissä olevan budgettimäärän. Määrää päivitetään säännöllisten menojen päivittyess
 //ja TODO listalla vähentää käyttäjän syöttämän päivän rahakulujen mukaan
 //TODO Tarkista, että ei tule lisättyä uutta budgettiasetusta, jos semmoinen jo löytyy!
 
-budgetSettingsRouter.post('/', async (request, response, next) => {
-    const body = request.body
+budgetSettingsRouter.post("/", async (request, response, next) => {
+  const body = request.body;
 
-    //Tarkistetaan käyttäjä tokenin avulla
-    const decodedToken = jwt.verify(request.token, process.env.SECRET)
-    if (!request.token || !decodedToken.id) {
-        return response.status(401).json({ error: 'token missing or invalid' })
-    }
+  //Tarkistetaan käyttäjä tokenin avulla
+  const decodedToken = jwt.verify(request.token, process.env.SECRET);
+  if (!request.token || !decodedToken.id) {
+    return response.status(401).json({ error: "token missing or invalid" });
+  }
 
-   const user = await User.findById(decodedToken.id)
+  const user = await User.findById(decodedToken.id);
 
-    try {
+  try {
+    // uuden budgettiasetuksen objekti
+    const budgetSetting = new BudgetSetting({
+      income: body.income,
+      savings: body.savings,
+      dailyBudget: ((body.income - body.income * body.savings) / 30).toFixed(2),
+      user: decodedToken.id,
+    });
 
-        // uuden budgettiasetuksen objekti
-        const budgetSetting  = new BudgetSetting({
-            income: body.income,
-            savings: body.savings,
-            dailyBudget: ((body.income - (body.income*body.savings))/30).toFixed(2),
-            user: decodedToken.id
-        })
+    //Luodaan budgetti asetus ja linkataan se käyttäjän kanssa.
+    const savedBudgetSetting = await budgetSetting.save();
+    user.budgetSettings = budgetSetting._id;
+    await user.save();
 
-        //Luodaan budgetti asetus ja linkataan se käyttäjän kanssa. 
-        const savedBudgetSetting = await budgetSetting.save()
-        user.budgetSettings = budgetSetting._id
-        await user.save()
-        
-        response.json(savedBudgetSetting.toJSON())
+    response.json(savedBudgetSetting.toJSON());
+  } catch (e) {
+    next(e);
+  }
+});
 
-    }catch (e) {
-        next(e)
-    }
-
-
-
-
-})
-
-module.exports = budgetSettingsRouter
+module.exports = budgetSettingsRouter;
