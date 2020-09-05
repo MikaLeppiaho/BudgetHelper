d18d9d3cdbc14fb6ff2a2dd466dc637721b63cc7
MikaLeppiaho <MikaLeppiaho@users.noreply.github.com>
2020-09-05 14:23:42 +0300

    new service path added

---
+++ build/precache-manifest.3d19f09dea5d09f70080aeb4553ea046.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0426dcf810ef0cd35bfb19403728292",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4919488c1469e574bca4",
+    "url": "/static/css/main.d0976ae5.chunk.css"
+  },
+  {
+    "revision": "a35d3881c03d665e5e09",
+    "url": "/static/js/2.8483b3c5.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.8483b3c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4919488c1469e574bca4",
+    "url": "/static/js/main.6307c26f.chunk.js"
+  },
+  {
+    "revision": "ac6ec9403aacc8d7dc8c",
+    "url": "/static/js/runtime-main.d2e0b102.js"
+  },
+  {
+    "revision": "804e0f28348dcf26934e1e4c70affe69",
+    "url": "/static/media/northfolk.804e0f28.jpg"
+  }
+]);
