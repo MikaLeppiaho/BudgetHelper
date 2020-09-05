e6aedc3407e1ba76f181ee482b2137924d819e7d
MikaLeppiaho <MikaLeppiaho@users.noreply.github.com>
2020-09-05 12:04:39 +0300

    deploying to heroku

--- utils/config.js
+++ utils/config.js
@@ -2,8 +2,11 @@ require('dotenv').config()
 
 let PORT = process.env.PORT
 let MONGODBURI = process.env.MONGODBURI
+if (process.env.NODE_ENV === 'development') {
+  MONGODBURI = process.env.MONGODBDEVURI
+}
 
 module.exports = {
-    MONGODBURI,
-    PORT
+  MONGODBURI,
+  PORT
 }
